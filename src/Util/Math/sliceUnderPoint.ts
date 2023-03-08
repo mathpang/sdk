20545e6906b712f8517605e12e16d5bf1d55eb4d
math-hsh <104745781+math-hsh@users.noreply.github.com>
2023-03-08 17:50:58 +0900

    Fix sliceUnderPoint

--- src/Util/Math/sliceUnderPoint.ts
+++ src/Util/Math/sliceUnderPoint.ts
@@ -1,3 +1,7 @@
 export function sliceUnderPoint(x: number, n: number, m: number) {
+  if (Number.isInteger(x)) {
+    return 0;
+  }
+
   return +(x.toFixed(m).split(".").pop() || "").slice(n - 1);
 }
