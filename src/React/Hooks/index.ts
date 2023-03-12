059d41ac6678555128c395876ab69367c861fa79
math-hsh <104745781+math-hsh@users.noreply.github.com>
2023-03-13 02:16:57 +0900

    Add comments

--- src/React/Hooks/index.ts
+++ src/React/Hooks/index.ts
@@ -1,5 +1,18 @@
 import { useForceRerender } from "./useForceRerender";
 
 export const ReactHooks = {
+  /**
+   * Forces a rerender of the component
+   * @returns {Function} A function that forces a rerender of the component
+   *
+   * @example
+   * ```
+   * const forceRerender = useForceRerender();
+   *
+   * useEffect(() => {
+   *  forceRerender();
+   * }, [someDependency]);
+   * ```
+   */
   useForceRerender,
 } as const;
