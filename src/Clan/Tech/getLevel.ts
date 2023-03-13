1702430ab24b940a4022ca44cad9e6577f9b2eaf
김진근 <jingeun@mathpang.com>
2023-03-13 13:20:02 +0900

    [Feature] add getLevelAndRestTechPoint

--- src/Clan/Tech/getLevel.ts
+++ src/Clan/Tech/getLevel.ts
@@ -1,34 +1,52 @@
 import { TechID } from "./TechID";
 
-export function getLevel({
+type MaxPointMap = {
+  [key in TechID]: number[];
+};
+
+const maxPointMap: MaxPointMap = {
+  EXTENDS_LAB: [0, 5000, 10000, 15000, 20000, 25000],
+  EXTENDS_FACTORY: [0, 5000, 10000, 15000, 20000, 25000],
+  EXTENDS_ROAD: [0, 5000, 10000, 15000, 20000, 25000],
+  FASHIONISTA: [0, 15000, 20000, 25000, 30000, 40000],
+  RESEARCH_LAB: [0, 10000, 15000, 20000, 25000, 35000],
+  RESEARCH_FACTORY: [0, 10000, 15000, 20000, 25000, 35000],
+  RESEARCH_ROAD: [0, 10000, 15000, 20000, 25000, 35000],
+  PET_LOVER: [0, 15000, 20000, 25000, 30000, 35000],
+};
+
+export function getLevelAndRestTechPoint({
   techId,
   totalPoint,
 }: {
   techId: TechID;
   totalPoint: number;
-}): number {
-  function getLevelByPointList(pointList: number[]) {
-    return Math.max(0, pointList.findIndex((point) => totalPoint < point) - 1);
+}): {
+  level: number;
+  restTechPoint: number;
+} {
+  const maxPoints = maxPointMap[techId];
+  let level = 0;
+  let restTechPoint = totalPoint;
+
+  for (const maxPoint of maxPoints) {
+    // maxPoint가 0일때는 무시
+    if (maxPoint === 0) {
+      continue;
+    }
+
+    // restTechPoint가 maxPoint보다 작으면 더이상 레벨이 올라가지 않기 때문에 break
+    if (maxPoint > restTechPoint) {
+      break;
+    }
+
+    // restTechPoint >= maxPoint이기 때문에 level을 1 올리고 restTechPoint를 현제 maxPoint만큼 줄인다
+    level += 1;
+    restTechPoint -= maxPoint;
   }
 
-  switch (techId) {
-    case "EXTENDS_LAB":
-    case "EXTENDS_FACTORY":
-    case "EXTENDS_ROAD":
-      return getLevelByPointList([0, 5000, 10000, 15000, 20000, 25000]);
-
-    case "FASHIONISTA":
-      return getLevelByPointList([0, 15000, 20000, 25000, 30000, 40000]);
-
-    case "RESEARCH_LAB":
-    case "RESEARCH_FACTORY":
-    case "RESEARCH_ROAD":
-      return getLevelByPointList([0, 10000, 15000, 20000, 25000, 35000]);
-
-    case "PET_LOVER":
-      return getLevelByPointList([0, 15000, 20000, 25000, 30000, 35000]);
-
-    default:
-      return 0;
-  }
+  return {
+    level,
+    restTechPoint,
+  };
 }
