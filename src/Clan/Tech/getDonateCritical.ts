e9d521bb0c90fde7f687343cf6b42daedaded1b5
math-hsh <104745781+math-hsh@users.noreply.github.com>
2023-07-08 19:27:43 +0900

    mathpang-sdk@0.4.15

--- src/Clan/Tech/getDonateCritical.ts
+++ src/Clan/Tech/getDonateCritical.ts
@@ -1,7 +1,11 @@
 import { MathUtil } from "../../Util/Math";
 
 // https://www.notion.so/mathpang/1b91c638fe47402b906653fb6e160da4
-export function getDonateCritical(userId: number, totalDonateAmount: number) {
+export function getDonateCritical(
+  userId: number,
+  totalDonateAmount: number,
+  criticalUpgradeStep = 0
+) {
   const seed = MathUtil.sliceUnderPoint(
     Math.sqrt(userId * totalDonateAmount + totalDonateAmount ** 2 + 1 / 7),
     9,
@@ -9,8 +13,8 @@ export function getDonateCritical(userId: number, totalDonateAmount: number) {
   );
 
   // seed 는 0 ~ 99 의 불규칙한 값
-  if (seed < 7) return "CRITICAL_10";
-  if (seed < 20) return "CRITICAL_5";
-  if (seed < 50) return "CRITICAL_2";
+  if (seed < 7 + criticalUpgradeStep * 0.4) return "CRITICAL_10";
+  if (seed < 20 + criticalUpgradeStep * 0.8) return "CRITICAL_5";
+  if (seed < 50 + criticalUpgradeStep * 1.2) return "CRITICAL_2";
   return null;
 }
